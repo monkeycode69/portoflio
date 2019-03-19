6c72e784835280cbc88f62da2a0575acd4583656
monkeycode69 <monkeycode69@gmail.com>
2019-03-19 12:00:23 +0900

    문구 교체
    문구 교체

--- js/main.js
+++ js/main.js
@@ -1,79 +1,97 @@
 // scroll smooth behavior
-$(document).ready(function () {
-    $('a').on('click', function (event) {
-        if (this.hash !== '') {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                window.location.hash = hash;
-            });
+$(document).ready(function() {
+  $("a").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function() {
+          window.location.hash = hash;
         }
-    });
+      );
+    }
+  });
 });
 
 // header nav
-$('.btn-nav').click(function () {
-    $('.nav-list').slideToggle();
-})
+$(".btn-nav").click(function() {
+  $(".nav-list").slideToggle();
+});
 
 // header nav indicator
-$(function () {
-    $('#progress').progress({
-        size: '3px',
-        position: 'bottom',
-        wapperBg: '#dbdbdb',
-        innerBg: '#956641',
-    });
+$(function() {
+  $("#progress").progress({
+    size: "3px",
+    position: "bottom",
+    wapperBg: "#dbdbdb",
+    innerBg: "#956641"
+  });
 });
-
 
 // slick
-$('.carousel').slick({
-    infinite: true,
-    slideToshow: 1,
-    slidesToscroll: 1,
-    arrows: false,
+$(".carousel").slick({
+  infinite: true,
+  slideToshow: 1,
+  slidesToscroll: 1,
+  arrows: false
 });
-$('.btn.btn-left').click(function () {
-    $('.carousel').slick('slickPrev');
-})
-$('.btn.btn-right').click(function () {
-    $('.carousel').slick('slickNext');
-})
+$(".btn.btn-left").click(function() {
+  $(".carousel").slick("slickPrev");
+});
+$(".btn.btn-right").click(function() {
+  $(".carousel").slick("slickNext");
+});
 
 // slick slider text
-$('#example').typer({
-    strings: [
-        "FRONT END",
-        "PRACTICE HARDER",
-        "BE BEST DEVELOPER"
-    ],
-    typeSpeed: 80,
-    backspaceSpeed: 20,
-    backspaceDelay: 800,
-    repeatDelay: 1000,
-    repeat: true,
-    autoStart: true,
+$("#example").typer({
+  strings: ["FRONT END", "ALWAYS DO YOUR", "BEST"],
+  typeSpeed: 80,
+  backspaceSpeed: 20,
+  backspaceDelay: 800,
+  repeatDelay: 1000,
+  repeat: true,
+  autoStart: true
 });
 
 // portfolio lightgallery
-$('.portfolio-img').lightGallery();
+$(".portfolio-img").lightGallery();
 
 // tab
-var $tab_list = $('.tab-list');
-$tab_list.find('.about-list').hide();
-$tab_list.find('.tab-list__item.active .about-list').show();
-$tab_list.find('button').click(function (e) {
-    e.preventDefault();
-    var $this = $(this);
-    $this.next('.about-list').show().parent('.tab-list__item').addClass('active').siblings('.tab-list__item').removeClass('active').find('.about-list').hide();
+var $tab_list = $(".tab-list");
+$tab_list.find(".about-list").hide();
+$tab_list.find(".tab-list__item.active .about-list").show();
+$tab_list.find("button").click(function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  $this
+    .next(".about-list")
+    .show()
+    .parent(".tab-list__item")
+    .addClass("active")
+    .siblings(".tab-list__item")
+    .removeClass("active")
+    .find(".about-list")
+    .hide();
 });
 
 function tab_menu(e) {
-    e.preventDefault();
-    var $this = $(this);
-    $this.next('ul').show().parent('li').addClass('active').siblings('li').removeClass('active').find('>ul').hide();
+  e.preventDefault();
+  var $this = $(this);
+  $this
+    .next("ul")
+    .show()
+    .parent("li")
+    .addClass("active")
+    .siblings("li")
+    .removeClass("active")
+    .find(">ul")
+    .hide();
 }
-$tab_list.find('ul>li>a').click(tab_menu).focus(tab_menu)
+$tab_list
+  .find("ul>li>a")
+  .click(tab_menu)
+  .focus(tab_menu);
